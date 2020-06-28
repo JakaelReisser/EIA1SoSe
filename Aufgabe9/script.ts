81144fc5a468f107656dbaa909b3542a47671e76
JakaelReisser <yannikcunha@web.de>
2020-06-28 14:49:10 +0200

    aufgabe 9

---
+++ Aufgabe9/script.ts
@@ -0,0 +1,41 @@
+// Naomi hat mir geholfen. uff³
+
+
+let todoListe = ["Medienanalyse Präsentation ", "EIA Hausaufgaben ", "Medienpsychologie ", "Medientechnik Präsentation"];
+
+
+
+window.addEventListener("load", function () {
+
+
+    
+
+    function drawList() {
+        Liste2.innerHTML = "";
+        for (var index = 0; index < todoListe.length; index++) {
+            Liste2.innerHTML += "<div>" + "<input type='checkbox'>" + todoListe[index] + "<i class='fas fa-trash-alt'id=delete></i>" + "</div>";
+        }
+    
+
+    
+        var total = document.querySelector("#mySpan");
+        total.innerHTML = todoListe.length;
+    }
+
+    var Liste2 = document.querySelector(".addlist");
+    drawList();
+
+
+
+    var input = document.querySelector("#newAdd");
+    console.log(input);
+
+    var addNewElement = document.querySelector("#button1");
+    addNewElement.addEventListener("click", function () {
+        todoListe.push(input.value);
+        drawList();
+        input.value = "";
+        console.log(todoListe);
+    });
+
+});
